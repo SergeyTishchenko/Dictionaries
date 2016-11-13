c6526cbbf08f69413bc27878d6846cb0121cdaf2
SergeyTishchenko <sergey_tishchenko@tut.by>
2016-11-13 16:47:58 +0300

    ready
    Dictionaries

---
+++ 4-10.py
@@ -0,0 +1,19 @@
+import random
+
+def solar_system():
+    return {'Mercury': 57.9,
+            'Venus': 67.2,
+            'Earth': 149.6,
+            'Mars': 227.9,
+            'Jupiter': 778.3,
+            'Saturn': 1427.0,
+            'Uranus': 2871.0,
+            'Neptune': 4497.1,
+            }
+
+
+if __name__ == '__main__':
+    dic = solar_system()
+    planet = random.choice(dic.keys())
+    distance = dic.get(planet)
+    print "Planet {name} is about {dist} mln km from the Sun".format(name=str(planet), dist=str(distance))
---
+++ 4-6.py
@@ -0,0 +1,13 @@
+def solar_system():
+    return {'Mercury': 57.9,
+            'Venus': 67.2,
+            'Earth': 149.6,
+            'Mars': 227.9,
+            'Jupiter': 778.3,
+            'Saturn': 1427.0,
+            'Uranus': 2871.0,
+            'Neptune': 4497.1,
+            }
+
+if __name__ == '__main__':
+    print solar_system()
